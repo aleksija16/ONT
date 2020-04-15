f3f225fe5a18ec764ab1bcc1a554239d33feef17
sarajovanovic <sara.jovanovic@elfak.rs>
2020-04-15 20:29:39 +0200

    15.04-Prototip-Znamenitosti-JednaZnamenitost-AddZnamenitost

--- Aplikacija/Prototip/Projekat_1/Pages/JednaZnamenitost.cshtml.cs
+++ Aplikacija/Prototip/Projekat_1/Pages/JednaZnamenitost.cshtml.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Projekat_1.Model;
 
 namespace Projekat_1
 {
     public class JednaZnamenitostModel : PageModel
     {
-        public void OnGet()
+        public OrganizacijaContext dbContext {get; set;}
+
+        [BindProperty]
+        public Znamenitosti TrenutnaZnamenitost {get; set;}
+
+        public JednaZnamenitostModel(OrganizacijaContext db)
         {
+            dbContext = db;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            TrenutnaZnamenitost = dbContext.Znamenitosti.Where(x=>x.Idznamenitosti == id).FirstOrDefault();
+            return Page();
         }
     }
 }
--- Aplikacija/Prototip/Projekat_1/Pages/Znamenitosti.cshtml.cs
+++ Aplikacija/Prototip/Projekat_1/Pages/Znamenitosti.cshtml.cs
@@ -4,13 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Projekat_1.Model;
 
 namespace Projekat_1
 {
     public class ZnamenitostiModel : PageModel
     {
+        public OrganizacijaContext dbContext;
+
+        public IList<Znamenitosti> SveZnamenitosti;
+
+        public ZnamenitostiModel(OrganizacijaContext db)
+        {
+            dbContext = db;
+        }
+
         public void OnGet()
         {
+            IQueryable<Znamenitosti> qZnamenitosti = dbContext.Znamenitosti;
+            SveZnamenitosti = qZnamenitosti.ToList();
         }
     }
 }
