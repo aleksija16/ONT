e25809960b6596bc0b18d02a77bf659f01482bab
aleksija16 <aleksandra.jovanovic@elfak.rs>
2020-04-19 22:20:01 +0200

    19.04-Prototip-Rezervacije(izmena)-JednaRezervacija

--- Aplikacija/Prototip/Projekat_1/Model/Rezervacije.cs
+++ Aplikacija/Prototip/Projekat_1/Model/Rezervacije.cs
@@ -6,13 +6,12 @@ namespace Projekat_1.Model
     public partial class Rezervacije
     {
         public uint Idrezervacije { get; set; }
+        public uint BrojOsoba {get; set;}
+        public DateTime DatumIzvodjenja { get; set; }
+ 
         public uint IdturisteRez { get; set; }
         public uint IdvodicaRez { get; set; }
-        public uint IdtureRez { get; set; }
-        public DateTime? DatumIzvodjenja { get; set; }
 
-        public virtual Ture IdtureRezNavigation { get; set; }
-        public virtual Turisti IdturisteRezNavigation { get; set; }
-        public virtual Vodici IdvodicaRezNavigation { get; set; }
+    
     }
 }
--- Aplikacija/Prototip/Projekat_1/Pages/Rezervacije.cshtml.cs
+++ Aplikacija/Prototip/Projekat_1/Pages/Rezervacije.cshtml.cs
@@ -4,13 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Projekat_1.Model;
 
 namespace Projekat_1
 {
     public class RezervacijeModel : PageModel
     {
+        public OrganizacijaContext dbContext;
+
+        public IList<Rezervacije> SveRezervacije;
+
+        public RezervacijeModel(OrganizacijaContext db)
+        {
+            dbContext = db;
+        }
+
         public void OnGet()
         {
+            IQueryable<Rezervacije> qRezervacije = dbContext.Rezervacije;
+            SveRezervacije = qRezervacije.ToList();
         }
     }
 }
