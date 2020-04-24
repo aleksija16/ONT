c6b771dc0eacb10086c43ea931ec5b699ddfa312
sarajovanovic <sara.jovanovic@elfak.rs>
2020-04-24 21:32:51 +0200

    24.04-Prototip-Prijava (izmene)

--- Aplikacija/Prototip/Projekat_1/Pages/Prijava.cshtml.cs
+++ Aplikacija/Prototip/Projekat_1/Pages/Prijava.cshtml.cs
@@ -7,23 +7,71 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Projekat_1.Model;
 
-namespace MyApp.Namespace
+namespace Projekat_1
 {
     public class PrijavaModel : PageModel
     {
         [BindProperty]
         public Korisnici TrenutniKorisnik {get; set;}
 
-        private readonly ILogger<PrijavaModel> _logger;
+        [BindProperty]
+        public int? SessionId {get; set;}
 
-        public PrijavaModel(ILogger<PrijavaModel> logger)
+        public enum TipKorisnika
         {
-            _logger = logger;
+            Admin,
+            Turista,
+            Vodic
+        }
+
+        [BindProperty]
+        public TipKorisnika TipTrenutnogKorisnika {get;set;}
+
+        public readonly OrganizacijaContext dbContext;
+
+        public PrijavaModel(OrganizacijaContext db)
+        {
+            dbContext = db;
+            SessionId = null;
         }
 
 
-        public void OnGet()
+        public IActionResult OnPost()
         {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }          
+            else
+            {
+                Korisnici PostojiKorisnik = dbContext.Korisnici.Where(x=>x.Username == TrenutniKorisnik.Username).FirstOrDefault();
+                if (PostojiKorisnik != null && PostojiKorisnik.Password == TrenutniKorisnik.Password)
+                {
+                    SessionId = (int?)PostojiKorisnik.IdKorisnika;
+                    
+                    if (PostojiKorisnik.IdTuristeKor!=null)
+                    {
+                        TipTrenutnogKorisnika = TipKorisnika.Turista;
+                    } 
+                    else if (PostojiKorisnik.IdVodicaKor != null) 
+                    {
+                        TipTrenutnogKorisnika = TipKorisnika.Vodic;
+                    }
+                    else 
+                    {
+                        TipTrenutnogKorisnika = TipKorisnika.Admin;
+                    }
+
+                    return Page();
+                }
+                else
+                {
+                    SessionId = -1;
+                    return Page();
+                }
+
+            }
+            
         }
     }
 }
