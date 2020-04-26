5ebb91425185756f5645de97f90163b2d2f68696
sarajovanovic <sara.jovanovic@elfak.rs>
2020-04-26 17:03:18 +0200

    26.04-Prototip-(modeli) Korisnici, Turisti

--- Aplikacija/Prototip/Projekat_1/Model/Turisti.cs
+++ Aplikacija/Prototip/Projekat_1/Model/Turisti.cs
@@ -5,18 +5,11 @@ namespace Projekat_1.Model
 {
     public partial class Turisti
     {
-        public Turisti()
-        {
-            Korisnici = new HashSet<Korisnici>();
-        }
-
         public uint IdTuriste { get; set; }
         public string ImeTuriste { get; set; }
         public string PrezimeTuriste { get; set; }
         public string Pol { get; set; }
         public DateTime? DatumRodjenja { get; set; }
         public string Email { get; set; }
-
-        public virtual ICollection<Korisnici> Korisnici { get; set; }
     }
 }
