b268a1d26149c282244a17b88111e13fe0f2df11
valentinajosanovic <valentina@elfak.rs>
2020-04-27 00:06:52 +0200

    27.04-Prototip-JedanVodic

--- Aplikacija/Prototip/Projekat_1/Pages/JedanVodic.cshtml.cs
+++ Aplikacija/Prototip/Projekat_1/Pages/JedanVodic.cshtml.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Projekat_1.Model;
 
 namespace Projekat_1
 {
     public class JedanVodicModel : PageModel
     {
-        public void OnGet()
+        public OrganizacijaContext dbContext {get; set;}
+
+        [BindProperty]
+        public Vodici TrenutniVodic {get; set;}
+
+        public JedanVodicModel(OrganizacijaContext db)
         {
+            dbContext = db;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            TrenutniVodic = dbContext.Vodici.Where(x=>x.IdVodica == id).FirstOrDefault();
+            return Page();
         }
     }
 }
