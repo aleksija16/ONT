e7971fe5a2a97029bae7ae63f649dc0459eadc27
aleksija16 <aleksandra.jovanovic@elfak.rs>
2020-04-27 20:14:06 +0200

    27.04-Prototip-HallOfFame

--- Aplikacija/Prototip/Projekat_1/Pages/HallOfFame.cshtml.cs
+++ Aplikacija/Prototip/Projekat_1/Pages/HallOfFame.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Projekat_1.Model;
 
 namespace Projekat_1
@@ -11,18 +12,18 @@ namespace Projekat_1
     public class HallOfFameModel : PageModel
     {
 
-        public OrganizacijaContext dbContext;
-
-        public IList<HallOfFame> SviUcesnici;
-
- public HallOfFameModel(OrganizacijaContext db)
+        public readonly OrganizacijaContext dbContext;
+        public int rednibroj;
+        public HallOfFameModel(OrganizacijaContext db)
         {
             dbContext = db;
         }
-        public void OnGet()
+         public IList<HallOfFame> HallOfFame { get;set; }
+        public async Task OnGetAsync()
         {
-             IQueryable<HallOfFame> qHallOfFame = dbContext.HallOfFame;
-            SviUcesnici = qHallOfFame.ToList();
+            HallOfFame = await dbContext.HallOfFame.OrderByDescending(x=>x.BrojPoena).ToListAsync();
+            rednibroj=0;
+
         }
     }
 }
