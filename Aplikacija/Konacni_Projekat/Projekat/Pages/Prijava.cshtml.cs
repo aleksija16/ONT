cf31cb42d3a119837d20d8f3e44912e652305aea
aleksija16 <aleksandra.jovanovic@elfak.rs>
2020-05-02 20:52:40 +0200

    02.05-Projekat-Prijava

--- Aplikacija/Konacni_Projekat/Projekat/Pages/Prijava.cshtml.cs
+++ Aplikacija/Konacni_Projekat/Projekat/Pages/Prijava.cshtml.cs
@@ -11,16 +11,37 @@ namespace Projekat.Pages
     public class PrijavaModel : PageModel
     {
         [BindProperty]
+        public Korisnici TrenutniKorisnik {get; set;}
+       [BindProperty]
         public int? SessionId {get; set;}
         public readonly OrganizacijaContext dbContext;
 
         public PrijavaModel(OrganizacijaContext db)
         {
             dbContext = db;
-            SessionId = null;
+           SessionId = null;
         }
-        public void OnGet()
+         public IActionResult OnPost()
         {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }          
+            else
+            {
+                Korisnici PostojiKorisnik = dbContext.Korisnici.Where(x=>x.Username == TrenutniKorisnik.Username).FirstOrDefault();
+                if (PostojiKorisnik != null && PostojiKorisnik.Password == TrenutniKorisnik.Password)
+                {
+                    
+                    return RedirectToPage("./Index");
+                }
+                else
+                {
+                SessionId = -1;
+                    return Page();
+                }
+
+            }
         }
     }
 }
