2f95ac0e4cb73d69b651c4000ec4a47dc72e95d4
sarajovanovic <sara.jovanovic@elfak.rs>
2020-05-10 16:53:22 +0200

    10.05-KonacniProjekat-KvizUradi

--- Aplikacija/KonacniProjekat/Pages/KvizUradi.cshtml.cs
+++ Aplikacija/KonacniProjekat/Pages/KvizUradi.cshtml.cs
@@ -5,21 +5,89 @@ using System.Threading.Tasks;
 using KonacniProjekat.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace KonacniProjekat
 {
     public class KvizUradiModel : PageModel
     {
+        [BindProperty]
         public int? SessionId {get; set;}
         public readonly OrganizacijaContext dbContext;
+
+        [BindProperty]
+        public int KvizId {get; set;}
+
+        [BindProperty]
+        public List<string> KorisnikoviOdgovori {get; set;}
+
+        public Kvizovi KvizZaIzradu {get; set;}
+
+        [BindProperty]
+        public IList<Pitanja> PitanjaZaIzradu {get; set;}
+
+        [BindProperty]
+        public int? BrojTacnihOdgovora {get; set;}
+
+        public HallOfFame RezultatIzradeKviza {get; set;}
 
         public KvizUradiModel(OrganizacijaContext db)
         {
             dbContext = db;
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGetAsync(uint? id, int kviz)
         {
+            SessionId = (int?)id;
+            KvizId = kviz;
+
+            KvizZaIzradu = await dbContext.Kvizovi.FindAsync((uint)kviz);
+
+            if (KvizZaIzradu == null)
+            {
+                return NotFound();
+            }
+
+            PitanjaZaIzradu = await dbContext.Pitanja.Where(x=>x.IdKviza == (uint) kviz).ToListAsync();
+
+
+            KorisnikoviOdgovori = new List<string>();
+            for(int i=0; i<PitanjaZaIzradu.Count(); i++)
+            {
+                KorisnikoviOdgovori.Add(i.ToString() +  "&^*");
+            }
+
+            return this.Page();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            PitanjaZaIzradu = await dbContext.Pitanja.Where(x=>x.IdKviza == (uint)KvizId).ToListAsync();
+
+            BrojTacnihOdgovora = 0;
+
+            for(var i=0; i<KorisnikoviOdgovori.Count(); i++)
+            {
+                if (KorisnikoviOdgovori[i] == PitanjaZaIzradu[i].TacanOdgovor)
+                {
+                    BrojTacnihOdgovora++;
+                }
+            }
+
+            /*
+            //Dodavanje u HOF tabelu
+            RezultatIzradeKviza.DatumRadjenja = DateTime.Now;
+            RezultatIzradeKviza.IdKvizaHof = (uint)KvizId;
+            RezultatIzradeKviza.IdTuristeHof = (uint)SessionId;
+            RezultatIzradeKviza.Poeni = ((int) BrojTacnihOdgovora) * 100 / KorisnikoviOdgovori.Count(); //Racuna se u %, mozda da budu konkretno poeni?
+            dbContext.HallOfFame.Add(RezultatIzradeKviza);*/
+
+
+            return this.Page();
+
+            //return RedirectToPage("./KvizJedan", new { id = SessionId, kviz = KvizId}); 
+            //Mozda kad se zavrsi kviz da se korisniku prikaze lista svih tacnih odgovora?
+            //Mozda da se samo prikaze rezultat na ovoj stranici?
         }
     }
 }
