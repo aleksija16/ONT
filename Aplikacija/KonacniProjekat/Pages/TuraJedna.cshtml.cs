94621879fffecb61479280529f13e5e9dde96cba
Aleksa Krstic <aleksa.krsticc@elfak.rs>
2020-05-12 20:20:45 +0200

    12.05-KonacniProjekat-TuraJedna

--- Aplikacija/KonacniProjekat/Pages/TuraJedna.cshtml.cs
+++ Aplikacija/KonacniProjekat/Pages/TuraJedna.cshtml.cs
@@ -5,21 +5,38 @@ using System.Threading.Tasks;
 using KonacniProjekat.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace KonacniProjekat
 {
     public class TuraJednaModel : PageModel
     {
         public int? SessionId {get; set;}
+
+        [BindProperty]
+        public Ture Ture {get; set;}
         public readonly OrganizacijaContext dbContext;
 
         public TuraJednaModel(OrganizacijaContext db)
         {
             dbContext = db;
+            SessionId = null;
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGetAsync(uint? id)
         {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Ture = await dbContext.Ture.FirstOrDefaultAsync(m => m.IdTure == id);
+
+            if (Ture == null)
+            {
+                return NotFound();
+            }
+            return Page();
         }
     }
 }
