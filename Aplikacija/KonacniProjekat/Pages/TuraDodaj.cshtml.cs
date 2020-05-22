7920d7ee3f5463678d4e69e45c3987b7cfd9ca53
valentinajosanovic <valentina@elfak.rs>
2020-05-22 20:46:32 +0200

    22.05-KonacniProjekat-TuraDodaj

--- Aplikacija/KonacniProjekat/Pages/TuraDodaj.cshtml.cs
+++ Aplikacija/KonacniProjekat/Pages/TuraDodaj.cshtml.cs
@@ -5,13 +5,14 @@ using System.Threading.Tasks;
 using KonacniProjekat.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace KonacniProjekat
 {
     public class TuraDodajModel : PageModel
     {
-        public int? SessionId {get; set;}
+        public int? SessionId { get; set; }
         public readonly OrganizacijaContext dbContext;
 
         public TuraDodajModel(OrganizacijaContext db)
@@ -19,19 +20,94 @@ namespace KonacniProjekat
             dbContext = db;
         }
 
-		[BindProperty]
-         public Ture NovaTura{get;set;}
+        [BindProperty]
+        public Ture NovaTura { get; set; }
 
-        public async Task<IActionResult> OnPostAsync(){
-            if(!ModelState.IsValid){
-                return Page();
-            }
-            else{
-                dbContext.Ture.Add(NovaTura);
-                await dbContext.SaveChangesAsync();
+        [BindProperty]
+        public int[] IzabraniDani { get; set; }
 
-                return RedirectToPage("./TuraSve");
+        public SelectList SviVodici { get; set; }
+
+        [BindProperty]
+        public string IzabraniVodic { get; set; }
+
+        [BindProperty]
+        public IList<Znamenitosti> SveZnamenitosti {get; set;}
+
+        [BindProperty]
+        public IList<int> IzabraneZnamenitosti {get; set;}
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            SessionId=id;
+
+            IQueryable<string> qVodici=dbContext.Vodici.Select(x=>x.ImeVodica);
+            SviVodici=new SelectList(await qVodici.ToListAsync());
+
+        
+           SveZnamenitosti = await dbContext.Znamenitosti.ToListAsync();
+
+            return this.Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return this.Page();
             }
+
+            string daniOdrzavanjaTure = "";
+            foreach (int i in IzabraniDani)
+            {
+                switch (i)
+                {
+                    case 1:
+                        daniOdrzavanjaTure += "Ponedeljak";
+                        break;
+                    case 2:
+                        daniOdrzavanjaTure += "Utorak";
+                        break;
+                    case 3:
+                        daniOdrzavanjaTure += "Sreda";
+                        break;
+                    case 4:
+                        daniOdrzavanjaTure += "Četvrtak";
+                        break;
+                    case 5:
+                        daniOdrzavanjaTure += "Petak";
+                        break;
+                    case 6:
+                        daniOdrzavanjaTure += "Subota";
+                        break;
+                    case 7:
+                        daniOdrzavanjaTure += "Nedelja";
+                        break;
+                }
+
+                daniOdrzavanjaTure += ", ";
+            }
+            daniOdrzavanjaTure = daniOdrzavanjaTure.Remove(daniOdrzavanjaTure.Length - 2);
+            NovaTura.DanOdrzavanja = daniOdrzavanjaTure;
+
+            IQueryable<Vodici> qIzabraniVodic=dbContext.Vodici.Where(x=>x.ImeVodica==IzabraniVodic);
+            NovaTura.IdVodicaNavigation=await qIzabraniVodic.FirstOrDefaultAsync();
+
+            /*
+            for(int i=IzabraneZnamenitosti.Count()-1; i>0; i--){
+            ZnamenitostiUTurama novaZut=new ZnamenitostiUTurama();
+            novaZut.IdTureZut=NovaTura.IdTure;
+            novaZut.IdZnamenitostiZut=(uint)IzabraneZnamenitosti[i];
+
+            dbContext.ZnamenitostiUTurama.Add(novaZut);
+            }
+*/
+            dbContext.Ture.Add(NovaTura);
+            await dbContext.SaveChangesAsync();
+
+            return RedirectToPage("./TuraSve");
+
         }
     }
 }
