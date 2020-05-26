d1b3583561a9f59bff7eba1e7fd773bede25a5ac
valentinajosanovic <valentina@elfak.rs>
2020-05-26 20:13:53 +0200

    26.05-KonacniProjekat-TuraObrisi-TuraSve-site.css

---
+++ Aplikacija/KonacniProjekat/Pages/TuraObrisi.cshtml.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using KonacniProjekat.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KonacniProjekat
+{
+    public class TuraObrisiModel : PageModel
+    {
+        [BindProperty]
+        public int? SessionId{get;set;}
+
+        [BindProperty]
+        public int TuraId{get;set;}
+
+        public Ture TuraZaBrisanje {get; set;}
+        public readonly OrganizacijaContext dbContext;
+
+        public TuraObrisiModel(OrganizacijaContext db){
+            dbContext=db;
+        }
+        public async Task<IActionResult>OnGetAsync(int tura)
+        {
+            TuraId=tura;
+
+            TuraZaBrisanje=await dbContext.Ture.Where( x => x.IdTure == (uint)TuraId).FirstOrDefaultAsync();
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(){
+    
+            TuraZaBrisanje=await dbContext.Ture.FindAsync((uint)TuraId);
+
+            if(TuraZaBrisanje==null){
+                return NotFound();
+            }
+
+            dbContext.Ture.Remove(TuraZaBrisanje);
+            await dbContext.SaveChangesAsync();
+
+            return RedirectToPage("./TuraSve");
+        }
+    }
+}
