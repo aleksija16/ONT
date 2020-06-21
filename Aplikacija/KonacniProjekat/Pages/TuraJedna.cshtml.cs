ebbeb2dec6e86c4ddacfd1887f9bbfbbdf097cc4
sarajovanovic <sara.jovanovic@elfak.rs>
2020-06-21 20:23:21 +0200

    21.06-KonacniProjekat-TuraJedna i site.css

--- Aplikacija/KonacniProjekat/Pages/TuraJedna.cshtml.cs
+++ Aplikacija/KonacniProjekat/Pages/TuraJedna.cshtml.cs
@@ -22,10 +22,28 @@ namespace KonacniProjekat
         }
 
         [BindProperty]
-        public Ture Ture {get; set;}
+        public Ture Tura {get; set;}
 
         [BindProperty]
         public Anketa RezultatiAnkete{get;set;}
+
+        [BindProperty]
+        public List<string> NajinteresantnijeZnamenitostiNaziv {get; set;}
+
+        // [BindProperty]
+        // public List<int> NajinteresantnijeZnamenitostiBrojPieChart {get; set;}
+
+        [BindProperty]
+        public List<string> NajdosadnijeZnamenitostiNaziv {get; set;}
+                
+        // [BindProperty]
+        // public List<int> NajdosadnijeZnamenitostiBrojPieChart {get; set;}
+
+        [BindProperty]
+        public IList<string> TipTuristeNaziv {get; set;}        
+
+        // [BindProperty]
+        // public List<int> TipTuristeBrojPieChart {get; set;}
 
         [BindProperty]
         public Vodici VodicTure{get;set;}
@@ -40,20 +58,88 @@ namespace KonacniProjekat
                  return NotFound();
             }
 
-            Ture = await dbContext.Ture.FirstOrDefaultAsync(m => m.IdTure == id);
-            
-            RezultatiAnkete=await dbContext.Anketa.FirstOrDefaultAsync(r=>r.IdTureAnk==id);
+            Tura = await dbContext.Ture.FirstOrDefaultAsync(m => m.IdTure == id);
+            if (Tura == null)
+            {
+                return NotFound();
+            }
 
-            VodicTure=await dbContext.Vodici.FirstOrDefaultAsync(e=>e.IdVodica==Ture.IdVodica);
+            IList<Anketa> SviRezultatiAnketa = await dbContext.Anketa.Where(x => x.IdTureAnk == id).ToListAsync();
+
+            if (SviRezultatiAnketa != null && SviRezultatiAnketa.Count() != 0) 
+            {
+                RezultatiAnkete = new Anketa();
+                RezultatiAnkete.FizickaZahtevnostTure = (uint?) SviRezultatiAnketa.Average(x => Convert.ToInt32(x.FizickaZahtevnostTure));
+                RezultatiAnkete.OrganizovanostTure = (uint?) SviRezultatiAnketa.Average(x => Convert.ToInt32(x.OrganizovanostTure));
+                RezultatiAnkete.InformisanostVodica = (uint?) SviRezultatiAnketa.Average(x => Convert.ToInt32(x.InformisanostVodica));
+                RezultatiAnkete.KonacnaOcena = (uint) SviRezultatiAnketa.Average(x => Convert.ToInt32(x.KonacnaOcena));
+            
+                var qNajinteresantnijeZnamenitosti = SviRezultatiAnketa.GroupBy(x => x.NajinteresantnijaZnamenitost)
+                    .Select(grupa => new { Znamenitost = grupa.Key, BrojPonavljanja = grupa.Count()});
+
+                if (qNajinteresantnijeZnamenitosti != null)
+                {
+                    List<string> qLista = qNajinteresantnijeZnamenitosti.OrderBy(x => x.BrojPonavljanja).Select(x => x.Znamenitost).ToList();
+                    if (qLista.Count() > 3)
+                        qLista.Take(3);
+
+
+                    NajinteresantnijeZnamenitostiNaziv = new List<string>(3);
+                    foreach(var znam in qLista)
+                    {
+                        NajinteresantnijeZnamenitostiNaziv.Add(znam);
+                    }                
+                    
+                }
+
+
+                var qNajdosadnijeZnamenitosti = SviRezultatiAnketa.GroupBy(x => x.NajdosadnijaZnamenitost)
+                    .Select(grupa => new { Znamenitost = grupa.Key, BrojPonavljanja = grupa.Count()});
+                
+                if (qNajdosadnijeZnamenitosti != null)
+                {
+                    List<string> qLista = qNajdosadnijeZnamenitosti.OrderBy(x => x.BrojPonavljanja).Select(x => x.Znamenitost).ToList();
+                    if (qLista.Count() > 3)
+                        qLista.Take(3);
+
+
+                    NajdosadnijeZnamenitostiNaziv = new List<string>(3);
+                    foreach(var znam in qLista)
+                    {
+                        NajdosadnijeZnamenitostiNaziv.Add(znam);
+                    }
+              
+                }
+
+                var qTipTuriste = SviRezultatiAnketa.GroupBy(x => x.TipTuriste)
+                    .Select(grupa => new { Tip = grupa.Key, BrojPonavljanja = grupa.Count()});
+                
+                if (qTipTuriste != null)
+                {
+                    List<string> qLista = qTipTuriste.OrderBy(x => x.BrojPonavljanja).Select(x => x.Tip).ToList();
+                    if (qLista.Count() > 3)
+                        qLista.Take(3);
+
+                    TipTuristeNaziv = new List<string>(3);
+                    foreach(var tip in qLista)
+                    {
+                        TipTuristeNaziv.Add(tip);
+                    }
+
+                    
+                }
+                
+            }
+            
+            
+
+            VodicTure=await dbContext.Vodici.FirstOrDefaultAsync(e=>e.IdVodica==Tura.IdVodica);
 
             IQueryable<ZnamenitostiUTurama> qZnamenitosti=dbContext.ZnamenitostiUTurama.Include(x=>x.IdZnamenitostiZutNavigation).Where(x=>x.IdTureZut==id);
             ZnamenitostiUTuri=await qZnamenitosti.Select(x=>x.IdZnamenitostiZutNavigation).ToListAsync();
 
 
-            if (Ture == null)
-            {
-                return NotFound();
-            }
+            
             return Page();
         }
     }
